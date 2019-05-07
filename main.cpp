2758ab1ae6e257f19242e6714be329ac0787afaf
Jean Tessier <jeant@clemson.edu>
2019-05-07 16:50:04 -0400

    Implment simple cli flag handling

--- main.cpp
+++ main.cpp
@@ -9,15 +9,34 @@
 #include "globals/globals.h"
 #include "fileHandler/fileHandler.h"
 
+void parseCommandLineFlags(int argc, char **argv, std::string &fileName, unsigned int &width, unsigned int &height)
+{
+    if (argc > 1)
+    {
+        fileName = argv[1];
+        if (argc > 3)
+        {
+            std::string convertedStr;
+            convertedStr = argv[2];
+            width = std::stod(convertedStr);
+            convertedStr = argv[3];
+            height = std::stod(convertedStr);
+        }
+    }
+}
+
 int main(int argc, char **argv)
 {
     std::shared_ptr<World> world = FileHandler::loadWorldFromFile("world.txt");
 
     Camera camera;
 
-    const unsigned int width = 600;
-    const unsigned int height = 400;
-    const std::string fileName = "img.ppm";
+    unsigned int width = 600;
+    unsigned int height = 400;
+    std::string fileName = "img.ppm";
+
+    parseCommandLineFlags(argc, argv, fileName, width, height);
+
     const auto pixels = camera.captureWorld((*world),
                                             width,
                                             height);
